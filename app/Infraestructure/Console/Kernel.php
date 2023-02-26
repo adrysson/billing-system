c0b09d86ae6b4395bfad5a1aea64aa1499598fb4
Adrysson <adrysson@doity.com.br>
2023-02-26 00:35:34 -0300

    Realizando agendamento da notificação da dívida

--- app/Infraestructure/Console/Kernel.php
+++ app/Infraestructure/Console/Kernel.php
@@ -2,6 +2,7 @@
 
 namespace App\Infraestructure\Console;
 
+use App\Application\NotifyDebts\DebtsNotifier;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
@@ -12,7 +13,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule): void
     {
-        // $schedule->command('inspire')->hourly();
+        $schedule->call(DebtsNotifier::class)->everyMinute();
     }
 
     /**
